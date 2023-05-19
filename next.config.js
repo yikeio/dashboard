382359121a27389999b4f6de2bd0df8da8672ff0
overtrue <anzhengchao@gmail.com>
2023-05-20 00:36:39 +0800

    fix: 修复 Build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh']
   },
   experimental: {
