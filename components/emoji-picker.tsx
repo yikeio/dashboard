87b616152e3a5011da1ddc5cf73c4e652eb9681b
overtrue <anzhengchao@gmail.com>
2023-05-26 12:09:18 +0800

    style: wip

--- components/emoji-picker.tsx
+++ components/emoji-picker.tsx
@@ -1,8 +1,7 @@
 import emojiData from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
-import { useState } from 'react';
 import { Button } from './ui/button';
-import classNames from 'classnames';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 
 interface EmojiProps {
   native: string;
@@ -12,21 +11,20 @@ export default function EmojiPicker(props: {
   children?: React.ReactNode;
   onSelect: (emoji: { native: string }) => void;
 }) {
-  const [isOpen, setIsOpen] = useState(false);
-
   const handleChange = (emoji: EmojiProps) => {
     props.onSelect(emoji);
-    setIsOpen(false);
   };
 
   const trigger = props.children || <Button variant={'outline'}>请选择</Button>;
 
   return (
-    <a className="cursor-default relative" onClick={() => setIsOpen(!isOpen)}>
-      {trigger}
-      <div className={classNames('absolute top-[100%]', { hidden: !isOpen })}>
-        <Picker data={emojiData} onEmojiSelect={handleChange} />
-      </div>
-    </a>
+    <>
+      <Popover>
+        <PopoverTrigger asChild>{trigger}</PopoverTrigger>
+        <PopoverContent className="w-auto p-0 border-none rounded-xl overflow-hidden">
+          <Picker data={emojiData} onEmojiSelect={handleChange} />
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
